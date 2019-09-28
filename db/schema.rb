f632e17fec5ae27cf9a73e5abbba3ec17135ea93
Brandon Teich <brandonteich123@gmail.com>
2019-09-28 22:39:06 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190729030650) do
+ActiveRecord::Schema.define(version: 20190804191500) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
